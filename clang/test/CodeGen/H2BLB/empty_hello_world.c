fa1cf545da2ebcdd6d4313b35599c23d46fba1fc
Shaoce SUN <sunshaoce@outlook.com>
2025-07-11 00:16:33 +0800

    Fix the issue `--target` cannot recognize `h2blb--darwin` (#11)

--- clang/test/CodeGen/H2BLB/empty_hello_world.c
+++ clang/test/CodeGen/H2BLB/empty_hello_world.c
@@ -1,5 +1,5 @@
 // REQUIRES: h2blb-registered-target
-// RUN: %clang --target=h2blb %s -o - -S | FileCheck %s
+// RUN: %clang --target=h2blb--darwin %s -o - -S | FileCheck %s
 
 // Check that we can connect clang from the driver (as opposed to cc1) all
 // the way to assembly code.
